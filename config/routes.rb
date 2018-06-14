ba948f7fce9cdcadb939d08188edd0c120b5826a
acdavi <davimilt@gmail.com>
2018-06-13 22:13:40 -0300

    Modelando a Home

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :cursos
 
 
+
   namespace :api do
     namespace :v1 do
         resources :contacts
@@ -11,4 +12,5 @@ Rails.application.routes.draw do
 
 
   root 'pages#home'
+
 end
